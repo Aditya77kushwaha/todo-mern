e74867002520aaf50d6df7cd0e3ffff6dbc2ddd2
Aditya77kushwaha <adityakushwaha2912@gmail.com>
2022-07-27 18:52:29 +0530

    adds postgresql to server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 8888
 const cors = require("cors")
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password: process.env.PASSWORD,
+    host: "localhost",
+    port: 5432,
+    database: "todopern"
+})
+
+
 
 app.use(express.json())
 app.use(cors())
@@ -11,8 +22,9 @@ app.get('/', (req, res) => {
 })
 
 app.post("/todo", async (req, res) => {
-    console.log(req.body.todo)
-    res.json({ msg:req.body.todo  })
+    // console.log(req.body.todo)
+    const newTodo = await pool.query("insert into todo (description) values($1)", [req.body.todo])
+    res.json({ msg: req.body.todo })
 })
 
 app.listen(port, () => {
