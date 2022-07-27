f8026bbaf55a67551eca28dc1507e6c557107d1e
aditya77kushwaha <adityakushwaha2912@gmail.com>
2022-07-28 01:01:04 +0530

    implements real-time todo creation and fetching;fixes .env

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,48 @@
-import GetTodo from "./components/gettodo/GetTodo";
-import PostTodo from "./components/posttodo/PostTodo";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [txt, settxt] = useState("");
+
+  const [todos, settodos] = useState({});
+  useEffect(() => {
+    axios.get("http://localhost:8888/todos").then((res) => {
+      settodos(res.data);
+    });
+  });
   return (
     <div className="App">
-      {/* <GetTodo /> */}
-      <PostTodo />
+      <div className="new-todo">
+        <input
+          type="text"
+          className="text"
+          value={txt}
+          onChange={(e) => settxt(e.target.value)}
+        />
+        <button
+          className="send"
+          onClick={() => {
+            axios
+              .post("http://localhost:8888/todo", {
+                desc: txt,
+              })
+              .then((res) => {
+                axios.get("http://localhost:8888/todos").then((res) => {
+                  console.log(res.data);
+                  settodos(res.data);
+                });
+                settxt("");
+              });
+          }}
+        >
+          Send
+        </button>
+        <ul>
+          {Object.keys(todos)?.map((x, ind) => {
+            return <li key={ind}>{todos[x].desc}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const port = 8888;
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Todo = require("./Model");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // const Pool = require("pg").Pool;
 
@@ -16,10 +19,10 @@ const Todo = require("./Model");
 // });
 
 mongoose.connect(
-  "mongodb+srv://user:user123@cluster0.czd6hbi.mongodb.net/?retryWrites=true&w=majority",
+  process.env.MONGODB_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
-    if (err) console.log(err); 
+    if (err) console.log(err);
     console.log("Connected to MongoDB");
   }
 );
@@ -31,8 +34,19 @@ app.get("/", (req, res) => {
   res.json("Hello world");
 });
 
+//get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//create a new todo
 app.post("/todo", async (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   // const newTodo = await pool.query("insert into todo (description) values($1)", [req.body.todo])
   const newTodo = new Todo(req.body);
   try {
@@ -40,7 +54,7 @@ app.post("/todo", async (req, res) => {
     res.status(200).json(savedTodo);
   } catch (err) {
     res.status(500).json(err);
-    console.log(err)
+    console.log(err);
   }
 });
 
