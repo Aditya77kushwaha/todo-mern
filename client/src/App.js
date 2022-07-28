49ff43cb39d5b91c249b9dd36b2969511b0623f4
aditya77kushwaha <adityakushwaha2912@gmail.com>
2022-07-28 15:52:13 +0530

    implements edit todo(client side)

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#root");
 
 function App() {
   const [txt, settxt] = useState("");
@@ -9,6 +23,21 @@ function App() {
       settodos(res.data);
     });
   });
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <div className="App">
       <div className="new-todo">
@@ -47,7 +76,6 @@ function App() {
                     axios
                       .delete(`http://localhost:8888/todos/${todos[x]._id}`)
                       .then((res) => {
-                        console.log(res.data);
                         axios.get("http://localhost:8888/todos").then((res) => {
                           settodos(res.data);
                         });
@@ -56,15 +84,47 @@ function App() {
                 >
                   Done
                 </button>
-                {/* <button
-                  className="edit"
-                  onClick={() => {
-                    !txt && settxt(prompt("Enter new todo", txt));
-                    console.log(txt);
-                  }}
-                >
+                <button className="edit" onClick={openModal}>
                   Edit
-                </button> */}
+                </button>
+                <Modal
+                  isOpen={modalIsOpen}
+                  onAfterOpen={afterOpenModal}
+                  onRequestClose={closeModal}
+                  style={customStyles}
+                  contentLabel="Example Modal"
+                >
+                  <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
+                    Edit `{todos[x].desc}`
+                  </h2>
+                  <button onClick={closeModal}>Cancel</button>
+
+                  <input
+                    type="text"
+                    className="text"
+                    value={txt}
+                    onChange={(e) => settxt(e.target.value)}
+                  />
+                  <button
+                    onClick={() => {
+                      axios
+                        .put(`http://localhost:8888/todos/${todos[x]._id}`, {
+                          desc: txt,
+                        })
+                        .then((res) => {
+                          settxt("");
+                          axios
+                            .get("http://localhost:8888/todos")
+                            .then((res) => {
+                              settodos(res.data);
+                            });
+                        });
+                      closeModal();
+                    }}
+                  >
+                    Okay
+                  </button>
+                </Modal>
               </li>
             );
           })}
